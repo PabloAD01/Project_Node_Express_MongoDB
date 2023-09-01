9aa7edd05c40f78d83127d69f4cb03912778b886
Pablo <piarayad@gmail.com>
2023-08-31 22:26:20 -0300

    feat: add functionality to log out button

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,13 +1,25 @@
 import React, { createContext, useContext, useState } from "react";
 import Wrapper from "../assets/wrappers/Dashboard";
 import { BigSidebar, Navbar, SmallSidebar } from "../components";
-import { Outlet } from "react-router-dom";
+import { Outlet, redirect, useLoaderData, useNavigate } from "react-router-dom";
+import customFetch from "../utils/customFetch";
+import {toast} from 'react-toastify';
+
+
+export const loader = async () => {
+  try {
+    const {data} = await customFetch.get('/users/current-user');
+    return data
+  } catch (error) {
+    return redirect('/')
+  }
+}
 
 const DashboardContext = createContext();
 
 const DashboardLayout = ({isDarkThemeEnabled}) => {
-  const user = { name: "pablo" };
-
+  const {user} = useLoaderData();
+  const navigate = useNavigate();
   const [showSidebar, setShowSidebar] = useState(false);
   const [isDarkTheme, setIsDarkTheme] = useState(isDarkThemeEnabled);
 
@@ -22,8 +34,11 @@ const DashboardLayout = ({isDarkThemeEnabled}) => {
     setShowSidebar(!showSidebar);
   };
 
-  const logoutUser = () => {
-    console.log("Logout user");
+  const logoutUser = async () => {
+    navigate('/');
+    await customFetch.get('/auth/logout');
+    toast.success('Logging out...');
+ 
   };
 
   return (
@@ -44,7 +59,7 @@ const DashboardLayout = ({isDarkThemeEnabled}) => {
           <div>
             <Navbar />
             <div className="dashboard-page">
-            <Outlet/>
+            <Outlet context={{user}}/>
             </div>
           </div>
         </main>
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Form, Link, useNavigation } from 'react-router-dom'
+import { Form, Link, useNavigation, redirect, useActionData } from 'react-router-dom'
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 import { FormRow, Logo } from "../components";
 import customFetch from '../utils/customFetch';
@@ -8,20 +8,26 @@ import {toast} from 'react-toastify';
 export const action = async ({ request }) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
-
+  const errors = {msg:''}
+  if (data.password.length < 3) {
+    errors.msg = 'Password must be at least 3 characters'
+    return errors
+  }
   try {
     await customFetch.post('/auth/login', data);
-    toast.success('User logged in successfully')
-    console.log(data);
+    toast.success('User created successfully')
     return redirect('/dashboard')
-  } catch (err) {
-    toast.error(err?.response?.data?.msg)
-    return err
+  } catch (error) {
+/*   toast.error(err?.response?.data?.msg) */
+    errors.msg = error.response.data.msg
+    return errors
   }
   
 
 }
 const Login = () => {
+  
+  const errors = useActionData()
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
 
@@ -30,6 +36,8 @@ const Login = () => {
     <Form method='post' className='form'>
       <Logo/>
       <h4>login</h4>
+      {errors?.msg && <p style={{color:'red'}}>{errors.msg}</p>}
+      <p></p>
       <FormRow type="email" name="email" defaultValue="pablo@gmail.com"/>
       <FormRow type="password" name="password" defaultValue="password123"/>
       <button type='submit' className='btn btn-block' disabled={isSubmitting} >{isSubmitting ? 'submitting' : 'submit'}</button>
