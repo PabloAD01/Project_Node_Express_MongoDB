51628230fff4cabde2cfb4a14c01650f3583ce72
Pablo Araya Diaz <piarayad@gmail.com>
2023-09-16 21:44:56 -0300

    feat: add Profile Page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import { FormRow } from '../components';
+import Wrapper from '../assets/wrappers/DashboardFormPage';
+import { useOutletContext } from 'react-router-dom';
+import { useNavigation, Form } from 'react-router-dom';
+import customFetch from '../utils/customFetch';
+import { toast } from 'react-toastify';
 
 const Profile = () => {
+  const {user} = useOutletContext();
+  const { name, lastName, email, location } = user;
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+
+
   return (
-    <h1>Profile</h1>
+    <Wrapper>
+      <Form method="post" className="form" encType='multipart/form-data'>
+        <h4 className='form-title'>Profile</h4>
+        <div className="form-center">
+          <div className="form-row">
+            <label htmlFor="avatar" className='form-label'>
+              Select an image file (max 0.5 MB)
+            </label>
+            <input 
+            type="file" 
+            id='avatar' 
+            name='avatar' 
+            className='form-input' 
+            accept='image/*'
+            />
+          </div>
+        <FormRow type="text" name="name" defaultValue={name} />
+        <FormRow type="text" name="lastName" labelText="lastName" defaultValue={lastName} />
+        <FormRow type="text" name="email" defaultValue={email} />
+        <FormRow type="text" name="location" defaultValue={location} />
+        <button type="submit" className="btn btn-block" disabled={isSubmitting}>
+          {isSubmitting ? 'Submitting...' : 'submit'}
+        </button>
+        </div>
+      </Form>
+    </Wrapper>
   )
 }
 
